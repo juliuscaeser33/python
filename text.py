1e3a590611df2c98ebe0fa9cc94c4a6407fc45b2
juliuscaeser33 <juliusmwangi33@gmail.com>
2025-08-02 17:15:04 +0200

    getting even numbers

--- text.py
+++ text.py
@@ -1,1 +1,6 @@
-print("this is a test code")
+data=[3,2,4,5,7,8,2,10]
+def getEvenNumber(data):
+        for numbers in data:
+            if numbers%2==0:
+              print(numbers)
+getEvenNumber(data)
